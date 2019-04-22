300d4e48e7c5eef1929119497c50d41071998c5d
Daniel Piña <32937837+DanpinaUCM@users.noreply.github.com>
2019-04-22 17:07:57 +0200

    UpdateProfile Completed

--- visuals/updateProfile.php
+++ visuals/updateProfile.php
@@ -11,7 +11,7 @@
 
 	<?php require("../includes/header.php")?>
 
-	<?php  //GET USER INFO
+	<?php  //GET CURRENT USER INFO
 		if(isset($_SESSION['login']) && isset($_SESSION['name'])){
 				$name = $_SESSION['name'];
         // ACCESS TO DB (SELECT)
@@ -30,16 +30,55 @@
 		}
 	?>
 
-	<?php
-		
+	<?php 		//UPDATING USER INFO
 
-	?>
+		if ($_SERVER["REQUEST_METHOD"] == "POST") {		//Getting values from form below
+			$formPassword = sha1(md5(test_input($_POST["password"])));
+			$formPhone = test_input($_POST["phone"]);
+			$formLocation = test_input($_POST["location"]);
+			$formImage = test_input($_POST["image"]);
+
+			//Core code
+			$somethingChanged = false;
+			if($formPassword == ""){
+					$query="UPDATE user SET password = '".$formPassword."' WHERE name = '$name'";
+					$res = $conn->query($query);
+					if (!$res) { echo "Sorry, we couldn't update your password."; } else $somethingChanged = true;
+			}
+			if($formPhone != $phone){
+					$query="UPDATE user SET phone = '".$formPhone."' WHERE name = '$name'";
+					$res = $conn->query($query);
+					if (!$res) { echo "Sorry, we couldn't update your phone."; } else $somethingChanged = true;
+			}
+			if($formLocation != $location){
+					$query="UPDATE user SET location = '".$formLocation."' WHERE name = '$name'";
+					$res = $conn->query($query);
+					if (!$res) { echo "Sorry, we couldn't update your location."; } else $somethingChanged = true;
+			}
+			if($formImage != $image){
+					$query="UPDATE user SET image = '".$formImage."' WHERE name = '$name'";
+					$res = $conn->query($query);
+					if (!$res) { echo "Sorry, we couldn't update your image."; } else $somethingChanged = true;
+			}
+
+			if($somethingChanged)
+				header( "Location: userProfile.php" );
+		}
+
+		function test_input($data) {
+			$data = trim($data);
+			$data = stripslashes($data);
+			$data = htmlspecialchars($data);
+			return $data;
+		}
+
+		?>
 
   <!-- HTML -->
 	<div class="row">
 		<div class="card">
 			<form method="post" action= "<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-				<p>Name: <input class="form" type="text" name="nombre" value="<?php echo $name ?>"</p>
+				<p>Name:</p> <p class="bubble"> <?php echo $name ?> </p>
 				<p>Password: <input class="form" type="password" name="password" value=""></p>
 				<p>Phone: <input class="form" type="text" name="phone" value="<?php echo $phone ?>"></p>
 				<p>Location: <input class="form" type="text" name="location" value="<?php echo $location ?>"></p>
